a0c3852160c6d878cccc39bd1494bee4037778e8
amjed201 <amjed4ever@hotmail.com>
2025-07-16 02:59:43 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Amjed Mohamed Babiker on 16/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
